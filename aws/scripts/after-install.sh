e5d0d57a361769e3aac00378ded65c75dc19ffb6
Yan Mao <y.mao@elsevier.com>
2024-01-09 22:42:33 +0100

    add aws scripts

---
+++ aws/scripts/after-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+
+# Copy war file from S3 bucket to tomcat webapp folder
+aws s3 cp s3://xfirefabanalyzerstack-webappdeploymentbucket-4hzzfft9vosf/xfire_fabrication_analyzer.zip /usr/local/xfire_fabrication_analyzer.zip
+unzip /usr/local/xfire_fabrication_analyzer.zip -d /usr/local/xfire_fabrication_analyzer
+
+# Ensure the ownership permissions are correct.
+#chown -R tomcat:tomcat /usr/local/tomcat9/webapps
