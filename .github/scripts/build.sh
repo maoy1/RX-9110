467e830c0d94c0ca458737a4be5dd9180c314857
Yan Mao <y.mao@elsevier.com>
2024-01-09 21:39:05 +0100

    update

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,6 +3,6 @@ set -xe
 
 # Maven is used to build  and create a war file.
 #mvn -Dmaven.test.skip=true clean install
-zip -r xfire_process_analyzer.zip *.py
+zip -r xfire_fabrication_analyzer.zip *.py
 
 
