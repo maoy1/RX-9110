0f44ce7712338676f3c87fec4fc72deba239aca4
Yan Mao <y.mao@elsevier.com>
2024-01-18 23:52:47 +0100

    update

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,6 +3,6 @@ set -xe
 
 # Maven is used to build  and create a war file.
 #mvn -Dmaven.test.skip=true clean install
-zip -r xfire_fabrication_analyzer.zip *.py
+zip -r xfire_fabrication_analyzer.zip requirements.txt  xfire-fabrication-process-analyzer aws
 
 
--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,8 +3,8 @@ set -xe
 echo "after-install.sh"
 ls
 # Copy war file from S3 bucket to tomcat webapp folder
-#aws s3 cp s3://xfirefabanalyzerstack-webappdeploymentbucket-4hzzfft9vosf/xfire_fabrication_analyzer.zip /usr/local/xfire_fabrication_analyzer.zip
-#unzip /usr/local/xfire_fabrication_analyzer.zip -d /usr/local/xfire_fabrication_analyzer
+aws s3 cp s3://xfirefabanalyzerstack-webappdeploymentbucket-4hzzfft9vosf/xfire_fabrication_analyzer.zip /usr/local/xfire_fabrication_analyzer.zip
+unzip /usr/local/xfire_fabrication_analyzer.zip -d /usr/local/xfire_fabrication_analyzer
 
 # Ensure the ownership permissions are correct.
-#chown -R tomcat:tomcat /usr/local/tomcat9/webapps
+chown -R ec2-user:-user /usr/local/xfire_fabrication_analyzer
