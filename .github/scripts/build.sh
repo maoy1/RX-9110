589a1cdf20408045b379f7a67a8b9819efa0a73e
Yan Mao <y.mao@elsevier.com>
2024-01-19 00:14:19 +0100

    update

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,6 +3,8 @@ set -xe
 
 # Maven is used to build  and create a war file.
 #mvn -Dmaven.test.skip=true clean install
+pwd
+ls
 zip -r xfire_fabrication_analyzer.zip requirements.txt  xfire-fabrication-process-analyzer aws
 
 
