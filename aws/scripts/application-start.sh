799294d2d45c6a887c431709d64771cbc0c6408e
Yan Mao <y.mao@elsevier.com>
2024-01-18 09:09:01 +0100

    CD

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -3,4 +3,5 @@ set -xe
 echo "application-start.sh"
 
 # Start Tomcat, the application server.
-service tomcat start
+#service tomcat start
+nohup python xfire-fabrication-process-analyzer/xfire_fabrication_dashboard.py &
