e3b6f22a827dd4e7837ce6972dae7a165daab22a
Yan Mao <y.mao@elsevier.com>
2024-01-19 07:51:54 +0100

    test

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,7 +4,7 @@ echo "after-install.sh"
 ls
 # Copy war file from S3 bucket to tomcat webapp folder
 aws s3 cp s3://xfirefabanalyzerstack-webappdeploymentbucket-4hzzfft9vosf/xfire_fabrication_analyzer.zip /usr/local/xfire_fabrication_analyzer.zip
-unzip /usr/local/xfire_fabrication_analyzer.zip -d /usr/local/xfire_fabrication_analyzer
+unzip /usr/local/xfire_fabrication_analyzer.zip -d /usr/local/codedeployresources
 
 # Ensure the ownership permissions are correct.
-chown -R ec2-user:ec2-user /usr/local/xfire_fabrication_analyzer
+chown -R ec2-user:ec2-user /usr/local/codedeployresources
