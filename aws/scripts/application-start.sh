7aacd5a7e5bbf0633871ff6a9044cc99254ef60e
Yan Mao <y.mao@elsevier.com>
2024-01-19 08:39:09 +0100

    test

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -5,6 +5,7 @@ echo "application-start.sh"
 # Start Tomcat, the application server.
 #service tomcat start
 pwd
-ls
+ls /usr/local/
+ls /usr/local/codedeployresources/
 nohup python /usr/local/codedeployresources/xfire_fabrication_dashboard.py &
 exit 1
